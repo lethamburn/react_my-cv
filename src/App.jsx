b05f9aa5df4fd55033e46227ffabf55e9803c168
Antonio Rosales <antoniorosalesmartinez90@gmail.com>
2021-07-14 16:01:27 +0200

    FEAT CV const

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,57 @@ import "./App.css";
 import { Hero, About, Education, Experience, More } from "./components";
 
 function App() {
-
-  const CV = [{
-    
-  }]
+  const CV = [
+    {
+      name: "Antonio",
+      adress: "Rosales Martínez",
+      city: "Madrid-Spain",
+      email: "antoniorosalesmartinez90@gmail.com",
+      birthDate: "26/10/1990",
+      phone: "(+34) 657511155",
+      aboutMe: [
+        {
+          info: "",
+          info: "",
+          info: "",
+          info: "",
+        },
+      ],
+      education: [
+        {
+          name: "",
+          date: "",
+          where: "",
+        },
+        {
+          name: "",
+          date: "",
+          where: "",
+        },
+      ],
+      experience: [
+        {
+          name: "",
+          date: "",
+          where: "",
+        },
+        {
+          name: "",
+          date: "",
+          where: "",
+        },
+      ],
+      languages: [
+        {
+          language: "",
+          wrlevel: "",
+          splevel: "",
+        },
+      ],
+      habilities: ["HTML5", "CSS3"],
+      volunteer: [{ name: "", where: "", description: "" }],
+    },
+  ];
   return (
     <div className="App">
       <Hero />
